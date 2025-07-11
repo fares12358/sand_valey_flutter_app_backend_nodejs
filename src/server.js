b4d8b5dfcf646a87d9d497a8ffd5ee1666bcf82f
fares mohamed <fm883254@gmail.com>
2025-07-12 00:55:53 +0300

    allow origin cors

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 dotenv.config();
 // Connect to MongoDB
