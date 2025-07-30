2f01e9693e192cbbc07a66a9dc9e10ab87baad62
fares mohamed <fm883254@gmail.com>
2025-07-30 07:37:31 +0300

    modify the token expire

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -4,6 +4,14 @@ export const generateAccessToken = (user) => {
   return jwt.sign(
     { userId: user._id, email: user.email, role: user.role || "user", },
     process.env.JWT_SECRET,
-    { expiresIn: '7d' }
+    { expiresIn: '365d' }
+  );
+};
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { userId: user._id },
+    REFRESH_SECRET,
+    { expiresIn: '30d' } // long-lived
   );
 };
