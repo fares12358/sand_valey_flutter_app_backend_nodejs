0b164926416abd9fbba06c5ac154adbe48d09158
fares mohamed <fm883254@gmail.com>
2025-07-30 08:22:17 +0300

    modify the middleware 3

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -4,14 +4,6 @@ export const generateAccessToken = (user) => {
   return jwt.sign(
     { userId: user._id, email: user.email, role: user.role || "user", },
     process.env.JWT_SECRET,
-    { expiresIn: '365d' }
-  );
-};
-
-export const generateRefreshToken = (user) => {
-  return jwt.sign(
-    { userId: user._id },
-    REFRESH_SECRET,
-    { expiresIn: '30d' } // long-lived
+    { expiresIn: '90d' }
   );
 };
